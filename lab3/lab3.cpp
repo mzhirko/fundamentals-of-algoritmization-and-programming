7581cd79f15d205e8e830e5efbb592b3840323f8
mzh <mzhirko@gmail.com>
2018-11-10 13:31:23 -0800

    lab3 fixed calc

---
+++ lab3/lab3.cpp
@@ -0,0 +1,84 @@
+//---------------------------------------------------------------------------
+
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <vcl.h>
+#include <math.h>
+#pragma hdrstop
+
+double factorial (int n);
+
+double resultFunction(double x, double y);
+//---------------------------------------------------------------------------
+
+#pragma argsused
+int main(int argc, char* argv[])
+{
+     // a value
+    double a = 0;
+
+    // b value
+    double b = 0;
+
+    double s = 0, y = 0, result = 0;
+
+    int n = 0;
+
+    double h = 0;
+
+    puts("Input interval range [a;b].");
+    puts("Input value left range a: ");
+    scanf("%lf", &a);
+
+    puts("Input value right range b: ");
+    scanf("%lf", &b);
+
+    puts("Input iteration step h: ");
+    scanf("%lf", &h);
+
+    puts("Input iteration count n: ");
+    scanf("%d", &n);
+
+    printf("Range [a;b] = [%8.llf;%8.4lf]\n", a, b );
+    printf("Step h = %8.4lf\n", h);
+    printf("Count n = %8.4lf\n", n);
+
+    for(double x = a ; x <= b; x = x + h) {
+
+        s = 0;
+
+        for(int k = 0; k <= n; k = k + 1) {
+            s = s + cos(k * x)/factorial(k);
+        }
+
+        y = pow(exp(1), cos(x)) * cos(sin(x));
+
+        result = resultFunction(y, s);
+
+        printf("s(%8.4lf) = %8.4lf y(%8.4lf) = %8.4lf result = %8.4lf \n", x, s, x, y, result);
+    }
+
+
+
+    puts("\nPress any key ... ");
+    getch();
+
+    return 0;
+}
+//---------------------------------------------------------------------------
+double factorial (int n) {
+    if (n == 0)
+        return 1;
+
+    return n * factorial(n - 1);
+}
+
+//////////////////////
+double resultFunction(double x, double y) {
+
+    return fabs(x-y);
+
+}
+
+
